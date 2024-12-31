9d3ee04b7a1202a19bb47af915a60f8cc69dcadb
MelisandeOnana <160874940+MelisandeOnana@users.noreply.github.com>
2024-12-31 13:29:07 +0100

    Add files via upload

---
+++ login.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+
+include 'includes/db_connect.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Récupérer les données du formulaire
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // Prévenir les injections SQL en utilisant des requêtes préparées
+    $sql = "SELECT * FROM administrateur WHERE nom_utilisateur = ? AND mot_de_passe = ?";
+    $stmt = $pdo->prepare($sql);
+    
+    // Exécution de la requête avec les paramètres
+    $stmt->execute([$username, $password]);
+
+    // Vérification du résultat
+    if ($stmt->rowCount() == 1) {
+        // Authentification réussie
+        $_SESSION['username'] = $username;
+        header("Location: admin/tableau_de_bord.php");
+        exit();
+    } else {
+        // Authentification échouée
+        echo "Nom d'utilisateur ou mot de passe incorrect.";
+    }
+}
+?>
